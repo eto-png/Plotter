f405edcacbf3f0f8952fccd5f14308847fe00c6b
Raphael Resende <raphaelresende10@gmail.com>
2026-09-16 16:28:26 -0300

    Update: atualização da lógica do construtor da classe ```Produtos.py```.

--- model/entities/Produtos.py
+++ model/entities/Produtos.py
@@ -1,8 +1,9 @@
 from model.entities.produto import Produto
 
 class Produtos:
-    def __init__(self, produto, quantidade, preco, desconto):
+    def __init__(self, produto : Produto, preco : float = 0, quantidade : int = 0, desconto : int = 0):
         self.produto : Produto = produto
         self.quantidade = quantidade
-        self.preco = preco
+        if preco == 0: self.preco = self.quantidade * self.produto.preco
+        else: self.preco = preco
         self.desconto = desconto
