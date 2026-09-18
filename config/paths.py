af1081ca1719d70f2bf9f36295d88e4229a38cf5
Raphael Resende <raphaelresende10@gmail.com>
2026-09-18 12:51:50 -0300

    Update: atualização do caminho da pasta de dashboards.

--- config/paths.py
+++ config/paths.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 
-
+# visuais
 ASSETS_DIR = ROOT_DIR / "assets"
 IMAGES_DIR = ASSETS_DIR / "images"
 FONTS_DIR = ASSETS_DIR / "fonts"
 STYLE_PATH = ASSETS_DIR / "style.qss"
 
-DASHBOARDS_DIR = ASSETS_DIR / "dashboards"
-
-VIEWS_DIR = ROOT_DIR / "view"
+# dados
+DATA_DIR = ROOT_DIR / "data"
+DASHBOARDS_DIR = DATA_DIR / "dashboards"
